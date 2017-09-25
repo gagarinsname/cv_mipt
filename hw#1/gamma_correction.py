012ef3d7018c10dd80dacd67f2c681fd5ec9efa6
Iurii Efimov <i.efimov@partner.samsung.com>
2017-09-25 15:05:34 +0300

    implemented gamma correction function

--- hw#1/gamma_correction.py
+++ hw#1/gamma_correction.py
@@ -1,9 +1,28 @@
 from __future__ import print_function
 from sys import argv
 import os.path
-
+import numpy as np
+import cv2
 
 def gamma_correction(src_path, dst_path, a, b):
+    src = cv2.imread(src_path)
+    assert src is not None
+
+    print('original shape:', src.shape)
+    print('type: %s' % type(src))
+    src = src.astype(float) / 255
+
+    dst = np.array(map(lambda x: a*x**b, src.ravel()))
+    dst_max = max(dst)
+    if dst_max > 0:
+        dst = dst / dst_max * 255
+    dst.shape = src.shape
+
+    print('result shape:', dst.shape)
+    print('type: %s' % type(dst))
+    
+    cv2.imwrite(dst_path, dst)
+
     pass
 
 

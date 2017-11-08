fb2b3971df46688be0a5444298f50b2307c6ef9b
Yuriy Efimov <ejs_XXvek@mail.ru>
2017-11-09 00:55:41 +0300

    added fixes

--- hw#4/ransac.py
+++ hw#4/ransac.py
@@ -4,81 +4,102 @@ import os.path, json
 import numpy as np
 from scipy.stats import chi2
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
 
 def plot_line_on_data(data, line):
     x, y = data[:, 0], data[:, 1]
-    max_y = max(y)
-    max_x = max(x)
-    xs = ys = data.shape[0]
-    a, b, c = line
 
-    plt.scatter(x, y)
+    max_x, min_x = max(x), min(x)
+    max_y, min_y = max(y), min(y)
+    y_lim = generate_y([min_x, max_x], (min_y, max_y), line)
 
-    if b == 0:
-        if a != 0:
-            xn = (-float(c)/a) * np.ones(xs)
-            yn = max_y * np.random.sample(xs)
-    else:
-        xn = max_x * np.random.sample(xs)
-        yn = np.array(map(lambda t: -float(a) / b * t - float(c) / b, xn)).reshape(xs)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.scatter(x, y, s=16, c='red')
+    plt_line = Line2D([min_x, max_x], y_lim, color='blue')
+    ax.add_line(plt_line)
 
-    plt.scatter(xn, yn)
     plt.show()
+
+
+def generate_y(x, y_range, line_params):
+    a, b, c = line_params
+
+    n = len(x)
+    x = np.array(x)
+
+    y_min, y_max = y_range
+    if a == 0:
+        return (float(-c) / b) * np.ones(n)
+    if b == 0:
+        return np.linspace(y_min, y_max, n)
+    return (x * a + c) / (-b)
 
 
 def generate_data(img_size, line_params, n_points, sigma, inlier_ratio):
     ys, xs = img_size
     a, b, c = line_params
+    x_min = y_min = 0
+    y_max, x_max = ys, xs
+
+    if b == 0:
+        x_avg = float(-c) / a
+        x_min_line, x_max_line = x_avg, x_avg
+        x_min = x_avg - xs / 2
+        x_max = x_avg + xs / 2
+    else:
+        y_low, y_high = generate_y([x_min, x_max], [y_min, y_max], line_params)
+        if abs(y_high - y_low) > y_max - y_min:
+            x_avg = (x_min + x_max) / 2
+            x_delta = (x_max - x_min) * (y_max - y_min) / abs(y_high - y_low)
+            x_min_line = int(x_avg - x_delta / 2)
+            x_max_line = int(x_avg + x_delta / 2)
+        else:
+            x_min_line = x_min
+            x_max_line = x_max
+
+    y_low, y_high = generate_y([x_min_line, x_max_line], (y_min, y_max), line_params)
+    y_mean = (y_low + y_high) / 2
+    y_min = y_mean - ys / 2
+    y_max = y_mean + ys / 2
 
     n_in = int(n_points * inlier_ratio)
     n_out = n_points - n_in
-
-    if b == 0:
-        if a == 0:
-            return -1
-        else:
-            x_in = (-float(c)/a) * np.ones(n_in)
-            y_in = ys * np.random.sample(n_in)
-    else:
-        x_in = xs * np.random.sample(n_in)
-        y_in = np.array(map(lambda x: -float(a)/b * x - float(c) / b + np.random.normal(0, sigma), x_in)).reshape(n_in)
+    x_in = np.linspace(x_min_line, x_max_line, n_in)
+    y_in = generate_y(x_in, (y_min, y_max), line_params)
+    y_noise = np.random.normal(loc=0, scale=sigma, size=n_in)
+    y_in = y_in + y_noise
 
     y_out, x_out = np.random.sample((2, n_out))
-    y_out *= ys
-    x_out *= xs
+    y_out = y_out * (y_max - y_min) + y_min
+    x_out = x_out * (x_max - x_min) + x_min
 
     x = np.hstack((x_in, x_out)).reshape(n_points, 1)
     y = np.hstack((y_in, y_out)).reshape(n_points, 1)
-
-    # plt.scatter(x, y)
-    # plt.show()
 
     return np.hstack((x, y))
 
 
 def compute_ransac_thresh(alpha, sigma):
-    # thresh^2 = F_inv(alpha) * sigma^2, where F is the cdf for chi-squared distribution
-    return np.sqrt(chi2.ppf(alpha, 1) * (sigma ** 2))
+    return np.sqrt(chi2.ppf(alpha, 2) * (sigma ** 2)) + 1e-6
 
 
 def compute_ransac_iter_count(conv_prob, inlier_ratio):
-    # should think more about it
     return int(np.log(1 - conv_prob)/np.log(1 - inlier_ratio**2))+1
 
 
 def get_model(p_1, p_2):
-    a = b = c = None
     x1, y1 = p_1
     x2, y2 = p_2
-    if y2 == y1:
-        a, b, c = 0, 1, -y1
-    elif x2 == x1:
-        a, b, c = 1, 0, -x1
-    else:
-        k = float(y2 - y1) / (x2 - x1)
-        b = y2 - k * x2
-        a, b, c = -k, 1, -b
+
+    a = (y1 - y2)
+    b = (x2 - x1)
+    c = (x1 * y2 - x2 * y1)
+    norm = a ** 2 + b ** 2
+    a /= norm
+    b /= norm
+    c /= norm
     model = a, b, c
     return model
 
@@ -87,15 +108,6 @@ def est_model(model, data, t):
     a, b, c = model
     res = list(map(lambda x: abs(a * x[0] + b * x[1] + c)/np.sqrt(a*a+b*b) <= t, data))
     return res
-
-def fit_model(data):
-    # data * z = b, where b = np.ones((data_size,1))
-
-    A_inv = np.matmul(data.T, data)
-    z = np.sum(np.matmul(np.linalg.inv(A_inv), data.T), 1)
-    a, b = z
-    model = a, b, 1
-    return model
 
 
 def compute_line_ransac(data, t, n):
@@ -112,17 +124,9 @@ def compute_line_ransac(data, t, n):
         n_inliers = sum(is_inlier)
 
         if n_inliers > prev_out:
-            data_fit = data[is_inlier, :]
-            fit_size = data_fit.shape[0]
-            indices = fit_size * np.random.random(2)
-            indices = indices.astype(int)
-            cpt1, cpt2 = data_fit[indices]
-            best_model = get_model(cpt1, cpt2)
-            new_model = fit_model(data_fit)
-            best_model = new_model
+            best_model = model
             prev_out = n_inliers
 
-    # print(prev_out)
     return best_model
 
 
@@ -157,7 +161,9 @@ def main():
 
     detected_line = compute_line_ransac(data, t, n)
     print(detected_line)
+
     plot_line_on_data(data, detected_line)
+
 
 
 if __name__ == '__main__':
